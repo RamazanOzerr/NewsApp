6c14f4f45c5e35a71d3dc180594a2e5a5965df14
Ramazan Ozer <ozerramazan991@gmail.com>
2024-12-27 22:24:16 +0300

    minor changes

---
+++ app/src/main/java/com/example/newsapp/util/URL.java
@@ -0,0 +1,12 @@
+package com.example.newsapp.util;
+
+public class URL {
+
+    public static final String API_KEY = "";
+
+//    public static final String API_KEY = "8df1ff2efe574d2096d946f396b1a158";
+
+    public static final String BASE_URL = "https://newsapi.org/";
+
+    public static final String AUTH_URL = "";
+}
